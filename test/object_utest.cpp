0f05091c028172c3b5c6e58eace72bb9c1540aad
Damian Pietruchowski <dpietruchowski@gmail.com>
2017-10-15 21:06:12 +0200

    [test] Update
    Signed-off-by: Damian Pietruchowski <dpietruchowski@gmail.com>

--- test/object_utest.cpp
+++ test/object_utest.cpp
@@ -4,8 +4,9 @@
 #include "types.h"
 #include "object.h"
 #include "gene.h"
-#include "terminalgene.h"
-#include "functiongene.h"
+#include "operation/operation.h"
+#include "operation/terminal.h"
+#include "operation/function.h"
 
 #include <iostream>
 
@@ -31,17 +32,11 @@ Double sub(const std::vector<Double*>& args);
 Double mult(const std::vector<Double*>& args);
 Double div(const std::vector<Double*>& args);
 
-using DoubleFunction = mep::Function<Double>;
-DoubleFunction addf = {add, "addition", 5};
-DoubleFunction subf = {sub, "subtraction", 2};
-DoubleFunction multf = {mult, "multiplication", 5};
-DoubleFunction divf = {div, "division", 2};
-
 using Object = mep::Object;
 using Gene = mep::Gene<Double>;
-using TerminalGene = mep::TerminalGene<Double>;
-using FunctionGene = mep::FunctionGene<Double>;
-using GenePtr = mep::GenePtr<Double>;
+using Terminal = mep::Terminal<Double>;
+using Function = mep::Function<Double>;
+using OperationPtr = mep::OperationPtr<Double>;
 
 class MEPCoreTest : public QObject
 {
@@ -51,58 +46,54 @@ public:
     MEPCoreTest();
 
 private Q_SLOTS:
-    void testCase1();
+    void simpleTest();
+  //  void operatorAndConstructorTests();
 };
 
 MEPCoreTest::MEPCoreTest()
 {
 }
 
-void MEPCoreTest::testCase1()
+void MEPCoreTest::simpleTest()
 {
-    std::vector<GenePtr> originals;
-    originals.emplace_back(new TerminalGene(0, {1}));
-    originals.emplace_back(new TerminalGene(1, {7.5}));
-    originals.emplace_back(new TerminalGene(2, {10}));
-    originals.emplace_back(new TerminalGene(3, {2.5}));
-    originals.emplace_back(new TerminalGene(4, {20}));
-    originals.emplace_back(new FunctionGene(5, addf));
-    originals.emplace_back(new FunctionGene(6, subf));
-    originals.emplace_back(new FunctionGene(7, multf));
-    originals.emplace_back(new FunctionGene(8, divf));
+    std::vector<OperationPtr> operations;
+    operations.emplace_back(new Terminal({10}, "ten"));
+    operations.emplace_back(new Terminal({1}, "one"));
+    operations.emplace_back(new Terminal({5}, "five"));
+    operations.emplace_back(new Function(add, "addition", 3));
+    operations.emplace_back(new Function(sub, "subtraction", 2));
+    operations.emplace_back(new Function(mult, "multiplication", 3));
+    operations.emplace_back(new Function(div, "division", 2));
 
-    for(const auto& g : originals) {
-        std::cout << g->write() << std::endl;
-        g->assess();
+    std::vector<Gene> chromosome;
+    chromosome.emplace_back(0, operations[0].get());
+    for (int i = 1; i < 10; ++i) {
+        uint idx = rand() % operations.size();
+        chromosome.emplace_back(i, operations[idx].get());
     }
-    std::vector<Gene*> args1;
-    args1.push_back(originals[0].get());
-    args1.push_back(originals[1].get());
-    args1.push_back(originals[2].get());
-    args1.push_back(originals[3].get());
-    args1.push_back(originals[4].get());
-    std::vector<Gene*> args2;
-    args2.push_back(originals[1].get());
-    args2.push_back(originals[3].get());
-    std::vector<Gene*> args3;
-    args3.push_back(originals[5].get());
-    args3.push_back(originals[8].get());
 
-    Gene* root = originals[6].get();
-    for(auto t1: args1) {
-        t1->run();
+    int i = 0;
+    for(auto& gene: chromosome) {
+        for(uint k = 0; k < gene.nArgs(); ++k)
+            gene.addChild(rand() % i);
+        ++i;
     }
-    for(auto t2: args2) {
-        t2->run();
+
+    for(const auto& gene: chromosome) {
+        std::cout << gene.write() << std::endl;
     }
-    args3[0]->run(args1);
-    args3[1]->run(args2);
-    root->run(args3);
-
-    std::cout << "The result is " << *args3[0]->result() << std::endl;
-    std::cout << "The result is " << *args3[1]->result() << std::endl;
-    std::cout << "The result is " << *root->result() << std::endl;
-
+    for(auto& gene: chromosome) {
+        std::vector<Gene*> args;
+        for(auto childIdx: gene.children())
+            args.push_back(&chromosome[childIdx]);
+        gene.run(args);
+    }
+    i = 0;
+    for(const auto& gene: chromosome) {
+        std::cout << "[" << i << "] " <<
+                     gene.result().value << std::endl;
+        ++i;
+    }
 
     QVERIFY2(true, "Failure");
 }
@@ -118,7 +109,7 @@ Double sub(const std::vector<Double*>& args) {
     return {**args[0] - **args[1]};
 }
 Double mult(const std::vector<Double*>& args) {
-    double product = 0;
+    double product = 1;
     for(const auto arg: args)
         product *= **arg;
     return {product};
@@ -129,4 +120,4 @@ Double div(const std::vector<Double*>& args) {
 
 QTEST_APPLESS_MAIN(MEPCoreTest)
 
-#include "unit_test.moc"
+#include "object_utest.moc"
